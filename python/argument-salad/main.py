755870acdc611145b1e488054f4690eb23a76afb
Emad Bin Abid <eabid@stellic.com>
2021-01-23 23:01:37 +0500

    Added function declaration.

--- python/argument-salad/main.py
+++ python/argument-salad/main.py
@@ -3,8 +3,10 @@ Python3 functions can take a mix of positional and named arguments. Define a fun
 and two optional arguments with default values.
 """
 
-def main():
+
+def argument_salad(required_arg, optional_arg_one=1, optional_arg_two=2):
     pass
 
+
 if __name__ == '__main__':
-    main()
+    argument_salad(0)
