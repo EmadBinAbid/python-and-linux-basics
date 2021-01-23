75562f5b5d9672ba23c0ceb2c589d0cf6ed3603f
Emad Bin Abid <eabid@stellic.com>
2021-01-23 23:09:40 +0500

    Renamed modules.

--- python/list_of_lists/main.py
+++ python/list_of_lists/main.py
@@ -4,8 +4,10 @@ says “List X” where X is the number of the inner list (i.e. “List 1”… 
 sum of all items in the inner list. Do this for all inner lists contained in the outer list.
 """
 
+
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
